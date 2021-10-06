f15aa74f3f76014e192a12099c3a3b72b7523610
yashaswi90 <yashaswi90@gmail.com>
2021-10-06 14:08:38 +0530

    <WIP> PromotionEngine Processor.

--- src/test/java/com/promo/engine/service/impl/PromotionEnginerProcessorServiceImplTest.java
+++ src/test/java/com/promo/engine/service/impl/PromotionEnginerProcessorServiceImplTest.java
@@ -15,24 +15,24 @@ import static org.junit.Assert.*;
 public class PromotionEnginerProcessorServiceImplTest {
 
     private PromotionEnginerProcessorService underTest;
-    HashMap<Character, ActivePromotionsItems> activePromotions=new HashMap<>();
-    HashMap<Character, Long> perUnitPrice=new HashMap<>();
+    HashMap<String, ActivePromotionsItems> activePromotions=new HashMap<>();
+    HashMap<String, Long> perUnitPrice=new HashMap<>();
 
     @Before
     public void setUp(){
 
         InventoryItemPrice inventoryItemPrice = new InventoryItemPriceServiceImpl();
-        inventoryItemPrice.registeredItemPrice('A', 50L);
-        inventoryItemPrice.registeredItemPrice('B', 30L);
-        inventoryItemPrice.registeredItemPrice('C', 20L);
-        inventoryItemPrice.registeredItemPrice('D', 15L);
+        inventoryItemPrice.registeredItemPrice("A", 50L);
+        inventoryItemPrice.registeredItemPrice("B", 30L);
+        inventoryItemPrice.registeredItemPrice("C", 20L);
+        inventoryItemPrice.registeredItemPrice("D", 15L);
         perUnitPrice=inventoryItemPrice.getItemPrice();
 
         ActivePromotionService promotionService =new ActivePromotionServiceImpl();
-        promotionService.addActivePromotion('A', new ActivePromotionsItems(130, 'A', 3));
-        promotionService.addActivePromotion('B', new ActivePromotionsItems(45, 'B', 2));
-        promotionService.addActivePromotion('C', new ActivePromotionsItems(30, 'C', 1));
-        promotionService.addActivePromotion('D', new ActivePromotionsItems(30, 'D', 1));
+        promotionService.addActivePromotion("A", new ActivePromotionsItems(130, "A", 3));
+        promotionService.addActivePromotion("B", new ActivePromotionsItems(45, "B", 2));
+        promotionService.addActivePromotion("C", new ActivePromotionsItems(30, "C", 1));
+        promotionService.addActivePromotion("D", new ActivePromotionsItems(30, "D", 1));
         activePromotions =promotionService.getActivePromotions();
 
         underTest=new PromotionEnginerProcessorServiceImpl();
@@ -40,15 +40,14 @@ public class PromotionEnginerProcessorServiceImplTest {
     @Test
     public void shouldApplyPromotions(){
 
-        HashMap<Character, Integer> items = new HashMap<>();
-        items.put('A', 3);
-        items.put('B', 5);
-        items.put('C', 1);
-        items.put('D', 1);
+        HashMap<String, Integer> items = new HashMap<>();
+        items.put("A", 3);
+        items.put("B", 5);
+        items.put("C", 1);
 
         long total = underTest.applyPromotionsToCartItems(items, perUnitPrice, activePromotions);
 
-        Assert.assertEquals(total,285L);
+        Assert.assertEquals(total,270L);
     }
 
 }
